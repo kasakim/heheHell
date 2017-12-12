5e89f29f898afc92418b587e7c343089d8ae968e
JunHyun Kim <kasa2848@hotmail.com>
2017-12-12 20:47:17 +0900

    2nd

---
+++ MVCpro01/MVCpro01/Controllers/xyzController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCpro01.Controllers
+{
+    public class xyzController : Controller
+    {
+        // GET: xyz
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
